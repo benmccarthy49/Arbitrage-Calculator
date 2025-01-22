0357362126c4e4753383dcd056c364b0cdc7ffb0
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-01-22 23:26:33 +0000

    Added some features to the frame

--- CalculatorFrame.java
+++ CalculatorFrame.java
@@ -1,6 +1,19 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public final class CalculatorFrame extends JFrame
 {
-        
+    private final JFrame frame = new JFrame();
+    private final JButton calculate = new JButton("Calculate");
+    private final JButton reset = new JButton("Reset");
+    private final JPanel panel = new JPanel();
+
+        public CalculatorFrame()
+        {
+            panel.add(calculate);
+            panel.add(reset);
+            frame.setContentPane(panel);
+            frame.setSize(300, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        }
 }
