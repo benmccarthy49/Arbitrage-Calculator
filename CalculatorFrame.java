3d63c9cfdbd4341e96446b5bb6e4cc629b1fba0c
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-01-23 22:49:32 +0000

    Added a few more buttons and changed the layout

--- CalculatorFrame.java
+++ CalculatorFrame.java
@@ -1,19 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public final class CalculatorFrame extends JFrame
 {
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("ArbitrageCalculator");
     private final JButton calculate = new JButton("Calculate");
     private final JButton reset = new JButton("Reset");
     private final JPanel panel = new JPanel();
+    private final JTextField totalPayout = new JTextField();
+    private final JTextField totalProfit = new JTextField();
+    private final JTextField ROI = new JTextField();
 
         public CalculatorFrame()
         {
+            panel.setLayout(null);
             panel.add(calculate);
             panel.add(reset);
+            panel.add(totalPayout);
+            panel.add(totalProfit);
+            panel.add(ROI);
+
+            calculate.setLocation(300, 300);
+            calculate.setSize(20, 20);
+
             frame.setContentPane(panel);
-            frame.setSize(300, 300);
+            frame.setSize(500, 500);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
+            frame.setResizable(false);
+            panel.setBackground(Color.green);
         }
 }
