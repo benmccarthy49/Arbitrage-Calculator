b767fbb7dcf83e45e336329e54768f2c5796e081
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-01-27 19:11:13 +0000

    Added reset button file

---
+++ reset.java
@@ -0,0 +1,17 @@
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+
+// public class reset implements ActionListener 
+// {
+//     private CalculatorFrame mainFrame;
+
+//     public reset()
+//     {
+//         this.mainFrame = mainFrame;
+//     }
+
+//     public void actionPerformed(ActionEvent e)
+//     {
+//         mainFrame.getInputValues();
+//     }
+// }
