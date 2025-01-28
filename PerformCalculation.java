e581a82038eb84330075185cd45b6471a8b4392a
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-01-28 22:53:24 +0000

    Added try catch block for invalid inputs

--- PerformCalculation.java
+++ PerformCalculation.java
@@ -6,33 +6,58 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class performCalculation implements ActionListener 
+public class PerformCalculation implements ActionListener 
 {
     private CalculatorFrame mainFrame;
     private List<Integer> submittedVals = new ArrayList<Integer>(); 
 
-    public performCalculation(CalculatorFrame mainframe)
+    public PerformCalculation(CalculatorFrame mainframe)
     {
         this.mainFrame = mainframe;
+        submittedVals.add(0);
+        submittedVals.add(0);
+        submittedVals.add(0);
     }
 
     @Override
     // Function for when the calculate button is pressed
     public void actionPerformed(ActionEvent e) 
     {
+        try
+        {
         mainFrame.calcButtonPressed();
         JTextField x = mainFrame.getBet1Field();
         int i = mainFrame.getInputValues(x);
-        this.submittedVals.add(i);
+        this.submittedVals.set(0, i);
         
         JTextField y = mainFrame.getBet2Field();
         int j = mainFrame.getInputValues(y);
-        this.submittedVals.add(j);
+        this.submittedVals.set(1, j);
 
         JTextField z = mainFrame.getStakeField();
         int k = mainFrame.getInputValues(z);
-        this.submittedVals.add(k);
+        this.submittedVals.set(2, k);
+        }
+        catch(NumberFormatException a)
+        {
+            mainFrame.setEnterOdds();
+            System.out.println("Exception Caught: " + e);
+        }
 
         System.out.println(submittedVals); //debugging line
+    }
+
+    public List<Integer> getSubmittedVals()
+    {
+        return submittedVals;
+    }
+
+    public void setSubmittedVals(Integer i, List<Integer> x)
+    {
+        for(int j=0; j<3; j++)
+        {
+            x.set(j, i);
+        }
+        System.out.println(x); //debugging line
     }
 }
