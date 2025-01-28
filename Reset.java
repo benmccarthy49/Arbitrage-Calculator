be3fea87f9cbd8648b94b1ef914a648ee1ca0bf8
Ben McCarthy <b.mccarthy4@lancaster.ac.uk>
2025-01-28 22:53:50 +0000

    Got the reset button working

---
+++ Reset.java
@@ -0,0 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class Reset implements ActionListener 
+{
+    private CalculatorFrame mainFrame;
+
+    public PerformCalculation c;
+
+    public Reset(CalculatorFrame mainFrame, PerformCalculation c)
+    {
+        this.mainFrame = mainFrame;
+        this.c = c;
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        mainFrame.getBet1Field().setText("");
+        mainFrame.getBet2Field().setText("");
+        mainFrame.getStakeField().setText("");
+        List<Integer> x = c.getSubmittedVals();
+        c.setSubmittedVals(0, x);
+    }
+}
